61100544005c96f780fe89cec2301ceeaedb98e5
Guilherme Berson <bersondev@gmail.com>
2020-06-06 22:28:14 -0300

    contador de pessoas + calculadora imc

---
+++ calculadora_de_imc/android/app/src/main/kotlin/br/com/bersoncrios/calculadoradeimc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.bersoncrios.calculadoradeimc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ contador_de_pessoas/android/app/src/main/kotlin/br/com/bersoncrios/contadordepessoas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.bersoncrios.contadordepessoas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
