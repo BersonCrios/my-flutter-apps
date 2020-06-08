80c6ce0a9916a610d519113bacb32e973f17cbdf
Guilherme Berson <bersondev@gmail.com>
2020-06-08 10:46:05 -0300

    Buscador de gifs

---
+++ giphy_go/android/app/src/main/kotlin/br/com/bersoncrios/giphygo/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.bersoncrios.giphygo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ giphy_go/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:giphygo/ui/home_page.dart';
+import 'package:giphygo/ui/gif_page.dart';
+
+main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
