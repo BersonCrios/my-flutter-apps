64206730e23d2c64a6902928897e9ae8e6b4e939
Guilherme Berson <bersondev@gmail.com>
2020-06-09 17:31:16 -0300

    Agenda de contatos

---
+++ contatinhos/android/app/src/main/kotlin/br/com/bersoncrios/contatinhos/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.bersoncrios.contatinhos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ contatinhos/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:contatinhos/home_page.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
